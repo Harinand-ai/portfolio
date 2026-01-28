90bb2a80c552f401f8a32b78cce698ca69b285df
Harinand Sreeshan <harinandsreeshan@gmail.com>
2026-01-28 14:29:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/harinand-ai/portfolio',   // ⚠️ replace with your repo name
+  base: '/portfolio/',   // ⚠️ replace with your repo name
 })
